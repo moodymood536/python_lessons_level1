fdcf28b85dbf15a2c17687fd113a03b9ecb99931
moodymood536 <martynov-nikita@yandex.ru>
2018-03-26 23:12:46 +0300

    finished lesson 2 normal tasks

--- lesson02/home_work/hw02_normal.py
+++ lesson02/home_work/hw02_normal.py
@@ -5,15 +5,19 @@
 # если такой корень вообще можно извлечь
 # Пример: Дано: [2, -5, 8, 9, -25, 25, 4]   Результат: [3, 5, 2]
 import math
+print("="*10, 'Задание 1', "="*10)
 lesson_2_norm_task1 = [2, -5, 8, 9, -25, 25, 4, 999, 125852, 1024, 121, 169, 225, 0, -25898]
 new_lesson_2_norm_task1 = []
 for i in range(len(lesson_2_norm_task1)):
     try:
-        if math.sqrt(lesson_2_norm_task1[i]) == int(math.sqrt(lesson_2_norm_task1[i])):
-            new_lesson_2_norm_task1.append(int(math.sqrt(lesson_2_norm_task1[i])))
+        math_sqrt_i = math.sqrt(lesson_2_norm_task1[i])
+        print('Квадратный корень числа {} равен {}'.format(lesson_2_norm_task1[i], math_sqrt_i))
+        if math_sqrt_i == int(math_sqrt_i):
+            new_lesson_2_norm_task1.append(int(math_sqrt_i))
     except ValueError:
         print('Из числа {} сложновато извлечь корень.'.format(lesson_2_norm_task1[i]))
-print(new_lesson_2_norm_task1)
+print('список извлесенных корней: ',new_lesson_2_norm_task1)
+print("="*10, 'Задание 2', "="*10)
 
 # Задача-2: Дана дата в формате dd.mm.yyyy, например: 02.11.2013.
 # Ваша задача вывести дату в текстовом виде, например: второе ноября 2013 года.
@@ -24,22 +28,25 @@ days = {'01': 'Первое', '02': 'Второе', '03': 'Третье', '04': 
 month = {'01': 'Января', '02': 'Февраля', '03': 'Марта', '04': 'Апреля', '05': 'Мая', '06': 'Июня', '07': 'Июля', '11': 'Ноября',}
 date_regexp = re.compile(r'[0-3][0-9]\.0[0-9]\.\d{4}|[0-3][0-9]\.1[1-2]\.\d{4}')
 # date_regexp = re.compile(r'[0-3][0-9]\.0[0-9]\.\d{4}|[0-3][0-9]\.1[1-2]\.\d{4}')
-date = "02.11.2013, 05.03.2025,, dsdfsdfdsf, 436y5fgs, 09.08.3695, "
+date = "02.11.2013, 05.03.2025,, dsdfsdfdsf, 436y5fgs, 09.08.3695, 26.39.2568"
 find_date = date_regexp.findall(date)
 result = date_regexp.findall(date)
-print(result)
+print('Найденные даты: ',result)
 for i in range(len(result)):
     print(days.get(result[i][:2]), month.get(result[i][3:5]), result[i][6:10], 'года')
+print("="*10, 'Задание 3', "="*10)
 # Задача-3: Напишите алгоритм, заполняющий список произвольными целыми числами
 # в диапазоне от -100 до 100. В списке должно быть n - элементов.
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
 import random
 random_list = []
-for i in range(-100, 100):
+n = 50 # amount of elements
+for i in range(n):
     random_list.append(random.randint(-100,100))
 print(random_list)
 print(len(random_list))
+print("="*10, 'Задание 4', "="*10)
 # Задача-4: Дан список, заполненный произвольными целыми числами.
 # Получите новый список, элементами которого будут: 
 # а) неповторяющиеся элементы исходного списка:
@@ -48,19 +55,19 @@ print(len(random_list))
 # например, lst = [1 , 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 4, 6]
 #a
 lst2 = list(set(random_list))
+print("Вычисляем неповторяющиеся элементы списка random_list - \n{},\n кол-во элементов сократилось до {}".format(lst2, len(lst2)))
 lst_example = [6, 2, 1, 2, 4, 5,  5, 2]
 lst_example2 = list(set(lst_example))
-print(lst2)
-print(lst_example2)
+print("Лист из примера для проверки: ",lst_example2)
 #b
-
-not_repeated_items = []
+#USE list.count
 lstb = [1 , 2, 4, 5, 6, 2, 5, 2]
-lstb.sort()
-print(lstb)
-for i in range(len(lstb)):
-    if lstb[i] != lstb[i+1]:
-        not_repeated_items.append(lstb[i])
-        print('Task 4b', not_repeated_items)
-
+lstb_not_repeated = []
+for i in lstb:
+    print("number of {} = {}".format(i, lstb.count(i)))
+    if lstb.count(i) == 1:
+        print('{} is only in the list, we will add it'.format(i))
+        lstb_not_repeated.append(i)
+        print(lstb_not_repeated)
+print(lstb_not_repeated)
 
