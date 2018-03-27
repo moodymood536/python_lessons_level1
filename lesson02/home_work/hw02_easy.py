5e8c1b10bf31cdc2b6aea770a89db38287682e35
moodymood536 <martynov-nikita@yandex.ru>
2018-03-27 19:38:23 +0300

    fixed lesson 2 easy task 2

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -21,10 +21,11 @@ for i in range(len(fruits)):
 
 fruits = [12345, 6789, 'apple', 'avocado', 'apricot', 'banana', 'date', 'apple']
 fruits_2 = [12345, '6789', 'apple', 'avocadddo', 'apdddricot', 'bdddanana', 'date']
-
-print(list(set(fruits)-set(fruits_2)))
+for item in fruits_2:
+    if item in fruits:
+        fruits.remove(item)
 print(fruits)
-print(fruits_2)
+
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
@@ -35,7 +36,7 @@ task_3_list = [4, 6, 8, 9, 1, 3, 7]
 new_task_3_list = []
 for i in range(len(task_3_list)):
     if task_3_list[i] % 2 == 0:
-        new_task_3_list.append(task_3_list[i]/4)
+        new_task_3_list.append(int(task_3_list[i]/4))
     else:
         new_task_3_list.append(task_3_list[i]*2)
 print(new_task_3_list)
