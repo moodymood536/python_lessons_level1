3c1dce2be2fe52ced12e88bc0f40a1f7032d9019
moodymood536 <martynov-nikita@yandex.ru>
2018-04-12 00:49:28 +0300

    6normal

--- lesson06/hw06_normal.py
+++ lesson06/hw06_normal.py
@@ -16,7 +16,7 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 class Student:
-    def __init__(self, name, surname, birth_date, school, class_room, teacher, mother, father):
+    def __init__(self, name, surname, birth_date, school, class_room, teacher, mother, father, education):
         self.name = name
         self.surname = surname
         self.birth_date = birth_date
@@ -25,3 +25,21 @@ class Student:
         self.teacher = teacher
         self.mother = mother
         self.father = father
+        self.education = education
+    def name_surname(self):
+        return f'{self.name} {self.surname}'
+    def student_info(self,student_name):
+        if self.teacher == "Иванов" and (self.class_room == "5А" or self.class_room == "6Б"):
+            return f'Учитель {self.teacher} не может преподавать у {self.class_room}'
+        else:
+            return f'{student_name} {self.class_room} {self.teacher} {self.education}'
+    def parents_name(self, name):
+        return f'{self.mother}'
+
+
+student_list = [Student("Александр", "Иванов", '10.11.1998', "8 гимназия", "5А", "Иванов", "Мамс", "Папс", "Физра, ИЗО"), Student("Петр", "Сидоров", '10.01.1995', "8 гимназия", "8Б", "Учитель", "Маааммм", "Пааапппп", "Физика, математика")]
+for student in student_list:
+    print(student.name_surname())
+for student in student_list:
+    print(student.student_info(student.name_surname()))
+print(student_list[0].parents_name())
